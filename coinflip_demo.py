89836eef8a6a2f6d78adfb803fdad4d5f0bcc564
Serenity Rune <36552964+j4lambert@users.noreply.github.com>
2025-04-07 11:50:06 -0400

    Update coinflip_demo.py

--- coinflip_demo.py
+++ coinflip_demo.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-perc_heads = st.number_input(label = 'Chance of Coins Landing on Heads',
-min_value = 0.0, max_value = 1.0, value = .5)
+st.title('Central Limit Theorem – Streamlit Demo')
+st.subheader('SerenityRune')
+st.write(('This app simulates a thousand coin flips using the chance of heads input below,'
+     'and then samples with replacement from that population and plots the histogram of the'
+     ' means of the samples in order to demonstrate the central limit theorem.'))
+perc_heads = st.number_input(
+    label='Chance of Coins Landing on Heads', 
+    min_value=0.0, 
+    max_value=1.0, 
+    value=.5)
 binom_dist = np.random.binomial(1, perc_heads, 1000)
 list_of_means = []
 for i in range(0, 1000):
-     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 fig, ax = plt.subplots()
-ax = plt.hist(list_of_means, range=[0,1])
+ax = plt.hist(list_of_means)
 st.pyplot(fig)
